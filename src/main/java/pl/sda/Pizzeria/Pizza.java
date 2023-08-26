87422eee803aed345bcc842931b805a163d49667
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-08-26 13:54:21 +0200

    Added the "Pizza" class to the project

---
+++ src/main/java/pl/sda/Pizzeria/Pizza.java
@@ -0,0 +1,26 @@
+package main.java.pl.sda.Pizzeria;
+
+import java.util.List;
+
+public class Pizza {
+    private String name;
+    private int bakingTime;
+    private List<String> ingredients;
+
+    public Pizza(String name, int bakingTime, List<String> ingredients) {
+        this.name = name;
+        this.bakingTime = bakingTime;
+        this.ingredients = ingredients;
+    }
+    public void prepare(){
+        System.out.println(String.format("In the process of making %s pizza, the following ingredients will be utilized: %s.",
+                name, ingredients.toString()));
+    }
+
+    public void bake() {
+        System.out.println(String.format("When making a %s pizza, set aside %d minutes.", name, bakingTime));}
+    public void box(){
+        System.out.println(String.format("Your %s pizza is being boxed up."));
+    }
+
+}
--- src/main/java/pl/sda/Pizzeria/PizzaFactory.java
+++ src/main/java/pl/sda/Pizzeria/PizzaFactory.java
@@ -1,4 +1,5 @@
 package main.java.pl.sda.Pizzeria;
 
 public class PizzaFactory {
+
 }
