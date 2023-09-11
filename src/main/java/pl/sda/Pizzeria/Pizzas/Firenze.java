4f229cc743d5c60c0e48ba5361b8c27418d7d35e
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-11 17:26:02 +0200

    adding Firenze class

--- src/main/java/pl/sda/Pizzeria/Pizzas/Firenze.java
+++ src/main/java/pl/sda/Pizzeria/Pizzas/Firenze.java
@@ -2,10 +2,11 @@ package main.java.pl.sda.Pizzeria.Pizzas;
 
 import main.java.pl.sda.Pizzeria.Pizza;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Firenze extends Pizza {
     public Firenze() {
-        super(name, bakingTime, ingredients);
+        super("Firenze", 20, Arrays.asList("Tomato sauce", "Blue cheese", "Broccoli", "Corn"));
     }
 }
