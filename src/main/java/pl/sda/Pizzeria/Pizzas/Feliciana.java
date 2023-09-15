e04e34b75ec72a862ac7f27c527f6170a730ed47
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-15 12:51:50 +0200

    adding Feliciana class

--- src/main/java/pl/sda/Pizzeria/Pizzas/Feliciana.java
+++ src/main/java/pl/sda/Pizzeria/Pizzas/Feliciana.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Feliciana extends Pizza {
+
     public Feliciana() {
         super("Feliciana", 19, Arrays.asList("Tomato sauce", "Cheese", "Ham", "Bacon", "Salami"));
     }
