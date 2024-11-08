75440c37b40ecc76f2eedeb12d56faa12447de54
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-08 10:24:06 +0100

    adding mail feature

--- src/main/java/org/example/house_forge/client/controller/ClientController.java
+++ src/main/java/org/example/house_forge/client/controller/ClientController.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import org.example.house_forge.client.model.ClientModel;
 import org.example.house_forge.client.service.ClientService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController("/")
 @AllArgsConstructor
 public class ClientController {
