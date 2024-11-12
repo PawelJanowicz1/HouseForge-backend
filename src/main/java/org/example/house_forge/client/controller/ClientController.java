00ca1e7335fc88c4388321b2f50bf4bfbbed6a75
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-12 20:11:35 +0100

    changes

--- src/main/java/org/example/house_forge/client/controller/ClientController.java
+++ src/main/java/org/example/house_forge/client/controller/ClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://budowa-domow-trojmiasto.pl"})
 @RestController("/")
 @AllArgsConstructor
 public class ClientController {
