c0fb5a04c5200519fd2bb63cded57ddf800944b9
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-14 15:36:49 +0100

    adding web configs

--- src/main/java/org/example/house_forge/config/WebConfig.java
+++ src/main/java/org/example/house_forge/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200", "https://www.budowa-domow-trojmiasto.pl")  // Dodaj localhost
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
