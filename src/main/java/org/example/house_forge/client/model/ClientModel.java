fcbd0fbf19f8a01efabc92efad7cbc8342d1db2c
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-16 09:51:42 +0100

    adding web configs

--- src/main/java/org/example/house_forge/client/model/ClientModel.java
+++ src/main/java/org/example/house_forge/client/model/ClientModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -15,17 +16,25 @@ public class ClientModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "name cannot be blank")
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 12, message = "Name cannot exceed 12 characters")
+    @Pattern(regexp = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$", message = "Name can only contain letters and spaces")
     private String name;
 
-    @Pattern(regexp = "^(?:\\+48\\s?\\d{3}\\s?\\d{3}\\s?\\d{3}|\\+48\\d{9}|\\d{9}|\\d{3}\\s?\\d{3}\\s?\\d{3})$", message = "Invalid phone number format")
+    @Pattern(
+            regexp = "^(?:\\+48\\s?\\d{3}\\s?\\d{3}\\s?\\d{3}|\\+48\\d{9}|\\d{9}|\\d{3}\\s?\\d{3}\\s?\\d{3})$",
+            message = "Invalid phone number format"
+    )
     private String phoneNumber;
 
-    @NotBlank(message = "email cannot be blank")
-    @Email
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    @Size(max = 25, message = "Email cannot exceed 25 characters")
     private String email;
 
-    @NotBlank(message = "message cannot be blank")
+    @NotBlank(message = "Message cannot be blank")
+    @Size(max = 500, message = "Message cannot exceed 500 characters")
+    @Pattern(regexp = "^[\\w\\s.,;:'\"-]+$", message = "Message contains invalid characters")
     private String message;
 
 
